3967aa4422164380857edd2321254f85f384413f
moonrailgun <moonrailgun@gmail.com>
2021-07-01 23:24:11 +0800

    chore: remove unuse code

--- jest.config.js
+++ jest.config.js
@@ -14,8 +14,6 @@ module.exports = {
   transform: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/test/fileTransformer.js',
-    '^.+\\.jsx?$': 'babel-jest', //这个是jest的默认配置
-    '^.+\\.ts?$': 'ts-jest' //typescript转换
   },
   transformIgnorePatterns: ['/node_modules/'],
   setupFilesAfterEnv: [],
