f6d5321c1e73f03c2ffa4987005962fa52e11348
moonrailgun <moonrailgun@gmail.com>
2021-09-02 16:49:54 +0800

    refactor: 调整bbcode纯文本换行问题

--- web/plugins/com.msgbyte.bbcode/src/tags/PlainText.tsx
+++ web/plugins/com.msgbyte.bbcode/src/tags/PlainText.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import type { TagProps } from '../bbcode/type';
 
 export const PlainText: React.FC<TagProps> = React.memo((props) => (
-  <pre style={{ display: 'inline' }}>{props.children}</pre>
+  <pre style={{ display: 'inline', whiteSpace: 'break-spaces' }}>
+    {props.children}
+  </pre>
 ));
 PlainText.displayName = 'PlainText';
