5da79b02c7b345013682b0f8790f9fc4f2735b16
moonrailgun <moonrailgun@gmail.com>
2021-09-05 14:00:15 +0800

    chore: 优化plugin install

--- web/plugins/install.js
+++ web/plugins/install.js
@@ -5,14 +5,16 @@ const execa = require('execa');
 
 const pluginDirs = glob.sync(path.join(__dirname, './com.msgbyte.*'));
 
+console.log(`开始安装插件依赖:\n${pluginDirs.join('\n')}`);
+
 Promise.all(
-  pluginDirs.map((dir) => {
+  pluginDirs.map((dir) =>
     execa('yarn', {
       cwd: dir,
       stdout: 'inherit',
       stderr: 'inherit',
-    });
-  })
+    })
+  )
 )
   .then(() => {
     console.log('插件依赖安装完毕');
