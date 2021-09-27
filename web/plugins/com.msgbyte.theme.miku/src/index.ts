987f2c9f8f2efbe4261850f5d72b1e14c3f3c787
moonrailgun <moonrailgun@gmail.com>
2021-09-27 20:48:55 +0800

    refactor: 修改样式文件为异步加载，因为图片会比较大

--- web/plugins/com.msgbyte.theme.miku/src/index.ts
+++ web/plugins/com.msgbyte.theme.miku/src/index.ts
@@ -1,7 +1,11 @@
 import { regPluginColorScheme } from '@capital/common';
-import './theme.less';
 
 regPluginColorScheme({
   label: 'Miku 葱',
   name: 'light+miku',
 });
+
+/**
+ * 异步加载以防止入口文件过大阻塞主应用加载(因为有图片)
+ */
+import('./theme.less');
