2627f1a86699e6c833df9f5f53d1491894c4420d
moonrailgun <moonrailgun@gmail.com>
2021-12-15 17:03:49 +0800

    chore: 插件静态资源解决方案

---
+++ web/.ministarrc.js
@@ -0,0 +1,20 @@
+const copy = require('rollup-plugin-copy');
+const path = require('path');
+
+module.exports = {
+  externalDeps: ['react'],
+  rollupPlugins: ({ pluginName }) => [
+    copy({
+      targets: [
+        {
+          src: path.resolve(
+            __dirname,
+            `./plugins/${pluginName}`,
+            './assets/**/*'
+          ),
+          dest: path.resolve(__dirname, `./dist/plugins/${pluginName}/public/`),
+        },
+      ],
+    }),
+  ],
+};
