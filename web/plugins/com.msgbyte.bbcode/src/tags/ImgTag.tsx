053a8572716718db8f7bc92d39fed624cda1e59f
moonrailgun <moonrailgun@gmail.com>
2021-12-15 16:09:00 +0800

    style: 修改bbcode插件，渲染图片时设定最大宽度
    并修复之前样式不生效的bug

--- web/plugins/com.msgbyte.bbcode/src/tags/ImgTag.tsx
+++ web/plugins/com.msgbyte.bbcode/src/tags/ImgTag.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { Image } from '@capital/component';
 import type { TagProps } from '../bbcode/type';
 
+const imageStyle: React.CSSProperties = {
+  maxHeight: 320,
+  maxWidth: 320,
+  width: 'auto',
+};
+
 export const ImgTag: React.FC<TagProps> = React.memo((props) => {
   const { node } = props;
   const text = node.content.join('');
   const url = node.attrs.url ?? text;
 
-  return <Image className="max-h-80 w-auto" preview={true} src={url} />;
+  return <Image style={imageStyle} preview={true} src={url} />;
 });
 ImgTag.displayName = 'ImgTag';
