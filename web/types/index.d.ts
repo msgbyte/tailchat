f12ef404fbdcdac0112af478409b41a40d5108e5
moonrailgun <moonrailgun@gmail.com>
2021-12-18 22:33:50 +0800

    test: fix test problem

--- web/types/index.d.ts
+++ web/types/index.d.ts
@@ -1,3 +1,4 @@
 declare module '*.jpg';
 declare module '*.svg';
 declare module '*.module.less';
+declare module '@bbob/parser'; // Work around for com.msgbyte.bbcode. ts-jest looks like cannot load plugins-self typedefinition
