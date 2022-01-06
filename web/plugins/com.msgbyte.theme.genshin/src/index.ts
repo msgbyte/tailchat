31e16f6cbdfbb8567a0c652e08c808168045dbf9
moonrailgun <moonrailgun@gmail.com>
2022-01-06 10:53:29 +0800

    feat: 原神主题增加琴皮肤

--- web/plugins/com.msgbyte.theme.genshin/src/index.ts
+++ web/plugins/com.msgbyte.theme.genshin/src/index.ts
@@ -5,6 +5,11 @@ regPluginColorScheme({
   name: 'light+genshin-hutao',
 });
 
+regPluginColorScheme({
+  label: '原神-琴',
+  name: 'light+genshin-jean',
+});
+
 /**
  * 异步加载以防止入口文件过大阻塞主应用加载(因为有图片)
  */
@@ -12,5 +17,8 @@ sharedEvent.on('loadColorScheme', (colorSchemeName) => {
   if (colorSchemeName === 'light+genshin-hutao') {
     console.log('正在加载胡桃主题...');
     import('./hutao/theme.less');
+  } else if (colorSchemeName === 'light+genshin-jean') {
+    console.log('正在加载琴主题...');
+    import('./jean/theme.less');
   }
 });
