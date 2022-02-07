df28455b87bbc9abd053b9c16fa372cbd1631318
moonrailgun <moonrailgun@gmail.com>
2022-02-07 17:29:21 +0800

    feat: 原神主题增加安柏 莫娜 罗莎莉亚配色方案

--- web/plugins/com.msgbyte.theme.genshin/src/index.ts
+++ web/plugins/com.msgbyte.theme.genshin/src/index.ts
@@ -10,6 +10,21 @@ regPluginColorScheme({
   name: 'light+genshin-jean',
 });
 
+regPluginColorScheme({
+  label: '原神-安柏',
+  name: 'light+genshin-amber',
+});
+
+regPluginColorScheme({
+  label: '原神-莫娜',
+  name: 'light+genshin-mona',
+});
+
+regPluginColorScheme({
+  label: '原神-罗莎莉亚',
+  name: 'light+genshin-rosaria',
+});
+
 /**
  * 异步加载以防止入口文件过大阻塞主应用加载(因为有图片)
  */
@@ -20,5 +35,14 @@ sharedEvent.on('loadColorScheme', (colorSchemeName) => {
   } else if (colorSchemeName === 'light+genshin-jean') {
     console.log('正在加载琴主题...');
     import('./jean/theme.less');
+  } else if (colorSchemeName === 'light+genshin-amber') {
+    console.log('正在加载安柏主题...');
+    import('./amber/theme.less');
+  } else if (colorSchemeName === 'light+genshin-mona') {
+    console.log('正在加载莫娜主题...');
+    import('./mona/theme.less');
+  } else if (colorSchemeName === 'light+genshin-rosaria') {
+    console.log('正在加载罗莎莉亚主题...');
+    import('./rosaria/theme.less');
   }
 });
