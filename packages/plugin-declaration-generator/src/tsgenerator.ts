ee0a0972b54b73305733aad6f62ea9dd158f57ce
moonrailgun <moonrailgun@gmail.com>
2022-03-23 11:29:52 +0800

    chore: dts function generator and comments

--- packages/plugin-declaration-generator/src/tsgenerator.ts
+++ packages/plugin-declaration-generator/src/tsgenerator.ts
@@ -2,6 +2,15 @@ import ts from 'typescript';
 import fs from 'fs-extra';
 
 /**
+ * Tools: https://ts-ast-viewer.com/
+ */
+
+interface ExportModuleItem {
+  name: string;
+  comment?: string;
+}
+
+/**
  * 解析文件
  */
 export function parseFile(filePath: string, options: ts.CompilerOptions) {
@@ -10,18 +19,51 @@ export function parseFile(filePath: string, options: ts.CompilerOptions) {
   const service = ts.createLanguageService(host, ts.createDocumentRegistry());
   const program = service.getProgram();
 
-  const exportModules: string[] = [];
-  program?.getSourceFile(filePath)?.forEachChild((node) => {
+  const exportModules: ExportModuleItem[] = [];
+  const sourceFile = program?.getSourceFile(filePath);
+  sourceFile?.forEachChild((node) => {
     if (ts.isExportDeclaration(node)) {
+      // 如果为导出声明: export { foo } from 'foo'
       node.exportClause?.forEachChild((exportSpec) => {
         if (ts.isExportSpecifier(exportSpec)) {
-          exportModules.push(exportSpec.name.text);
+          exportModules.push({
+            name: exportSpec.name.text,
+            // comment:
+          });
         }
       });
+    } else if (isExportFunc(node)) {
+      // 如果是方法导出: export function foo() {}
+      if (node.name) {
+        exportModules.push({
+          name: node.name.text,
+          comment: getNodeComments(node),
+        });
+      }
     }
   });
 
   return { exportModules };
+}
+
+function isExportFunc(node: ts.Node): node is ts.FunctionDeclaration {
+  if (ts.isFunctionDeclaration(node)) {
+    if (node.modifiers) {
+      return node.modifiers.some((m) => m.kind === ts.SyntaxKind.ExportKeyword);
+    }
+  }
+
+  return false;
+}
+
+function getNodeComments(node: ts.Node): string | undefined {
+  const comments = ts.getSyntheticLeadingComments(node);
+
+  if (!comments) {
+    return undefined;
+  }
+
+  return comments.map((c) => c.text).join('\n');
 }
 
 class FileServiceHost implements ts.LanguageServiceHost {
--- packages/plugin-declaration-generator/test/demo/index.ts
+++ packages/plugin-declaration-generator/test/demo/index.ts
@@ -1,2 +1,9 @@
 export { foo } from '@/foo';
 export { bar } from '@/bar';
+
+/**
+ * Root export
+ */
+export function main() {
+  console.log('main');
+}
