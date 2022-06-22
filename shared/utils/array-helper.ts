1fa779beaa492bae95f707e807fa118a47444fc4
moonrailgun <moonrailgun@gmail.com>
2022-06-22 15:10:31 +0800

    style: add comment for joinArray

--- shared/utils/array-helper.ts
+++ shared/utils/array-helper.ts
@@ -3,6 +3,10 @@ import _flatten from 'lodash/flatten';
 /**
  * 类似于join，但是返回一个数组
  * join会将元素强制转化为字符串
+ *
+ * 改函数可以用于join ReactNode
+ *
+ * @example joinArray([1, 2, 3], '5') => [1, '5', 2, '5', 3]
  */
 export function joinArray<T, K>(arr: T[], separator: K): (T | K)[] {
   return _flatten(
