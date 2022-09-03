9e618c5828e284d0d4353d5920d47d9f5025c2e7
moonrailgun <moonrailgun@gmail.com>
2022-09-04 00:02:03 +0800

    fix: 修复服务端插件路径不正确的问题

--- client/shared/manager/service.ts
+++ client/shared/manager/service.ts
@@ -2,7 +2,7 @@ import { buildRegFnWithEvent } from './buildReg';
 
 /**
  * 获取服务器地址相关逻辑
- * @default "/" (前后端一体)
+ * @default window.location.origin (前后端一体)
  */
 export const [getServiceUrl, setServiceUrl, onServiceUrlChange] =
-  buildRegFnWithEvent<() => string>('serverUrl', () => '/');
+  buildRegFnWithEvent<() => string>('serverUrl', () => window.location.origin);
