df3630ba93c9527d75c1f7d57d8d76b7044e86e9
moonrailgun <moonrailgun@gmail.com>
2022-10-11 15:47:39 +0800

    feat(admin): basic tushan

---
+++ server/admin/src/resources/User.ts
@@ -0,0 +1,43 @@
+import { Entity, ObjectIdColumn, Column, PrimaryColumn } from 'tushan';
+
+@Entity({
+  name: 'users',
+})
+export class User {
+  @ObjectIdColumn()
+  _id!: string;
+
+  @Column()
+  username!: string;
+
+  @Column()
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column()
+  nickname!: string;
+
+  @Column()
+  discriminator!: string;
+
+  @Column({
+    default: false,
+  })
+  temporary!: boolean;
+
+  @Column()
+  avatar!: boolean;
+
+  @Column({
+    enum: ['normalUser', 'pluginBot', 'thirdpartyBot'],
+    default: 'normalUser',
+  })
+  type: string;
+
+  @Column({
+    default: {},
+  })
+  settings: string;
+}
