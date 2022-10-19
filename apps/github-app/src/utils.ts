abee334e585497ab6c30fbb8951fbe8c79396fcf
moonrailgun <moonrailgun@gmail.com>
2022-10-19 13:46:54 +0800

    refactor(github-app): 调整错误提示排版方式

--- apps/github-app/src/utils.ts
+++ apps/github-app/src/utils.ts
@@ -2,8 +2,8 @@ export const configPath = '.tailchat/topic.json';
 
 export function generateErrorBlock(err: unknown) {
   const detail = err instanceof Error ? err : new Error(String(err));
-  const errorBlock =
-    '```' + detail.name + detail.message + '\n' + (detail.stack ?? '') + '```';
+  const codesign = '```';
+  const errorBlock = `${codesign}\n${detail.name}${detail.message}\n${detail.stack}\n${codesign}`;
 
-  return `Tailchat occur error, please checkout your config in \`${configPath}\`! \nError:\nw${errorBlock}`;
+  return `Tailchat occur error, please checkout your config in \`${configPath}\`! \n${errorBlock}`;
 }
