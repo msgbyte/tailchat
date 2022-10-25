3fc8f4fbdc01f451d66288934aab0e00ce978955
moonrailgun <moonrailgun@gmail.com>
2022-10-25 19:43:44 +0800

    chore: 统一jest版本为27, 修复单元测试问题

--- client/web/test/fileTransformer.js
+++ client/web/test/fileTransformer.js
@@ -2,8 +2,10 @@ const path = require('path');
 
 module.exports = {
   process(src, filename, config, options) {
-    return {
-      code: 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';',
-    };
+    const code =
+      'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
+
+    // return { code };
+    return code;
   },
 };
