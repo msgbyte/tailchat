eec05c708969ffaab780e0b3c1c82b5b57e25380
moonrailgun <moonrailgun@gmail.com>
2022-12-21 00:27:48 +0800

    feat: sentry增加replay集成

--- client/web/plugins/com.msgbyte.sentry/src/index.tsx
+++ client/web/plugins/com.msgbyte.sentry/src/index.tsx
@@ -4,7 +4,9 @@ import { sharedEvent } from '@capital/common';
 
 Sentry.init({
   dsn: 'https://177fd98a1e9e4deba84146a769633c32@o4504196236836864.ingest.sentry.io/4504196241293312',
-  integrations: [new BrowserTracing()],
+  replaysSessionSampleRate: 0,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [new BrowserTracing(), new Sentry.Replay()],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
