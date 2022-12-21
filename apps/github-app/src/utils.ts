fafb698784ba9aef691826ee72f7885f6988a01c
moonrailgun <moonrailgun@gmail.com>
2022-12-21 15:52:44 +0800

    chore: remove commitlint(because of its alias of @commitlint/cli)

--- apps/github-app/src/utils.ts
+++ apps/github-app/src/utils.ts
@@ -1,5 +1,9 @@
 export const configPath = '.tailchat/topic.json';
 
+/**
+ * 输入错误对象
+ * 返回一个详细信息的markdown字段
+ */
 export function generateErrorBlock(err: unknown) {
   const detail = err instanceof Error ? err : new Error(String(err));
   const codesign = '```';
