1d924f1692b048b69aac0ad0a8cae0075a08acff
moonrailgun <moonrailgun@gmail.com>
2022-12-25 15:51:18 +0800

    perf: sentry和posthog增加try...catch保护

--- client/web/plugins/com.msgbyte.posthog/src/index.tsx
+++ client/web/plugins/com.msgbyte.posthog/src/index.tsx
@@ -1,51 +1,55 @@
 import posthog from 'posthog-js';
 import { sharedEvent } from '@capital/common';
 
-posthog.init('phc_xRCv3qbbOBMQkz31kbYMngXxn7Ey5JMu0BZIFktO6km', {
-  api_host: 'https://app.posthog.com',
-  autocapture: false, // 关闭autocapture以节约事件用量
-  disable_session_recording: true, // 关闭自动录屏(不需要且一直报错)
-});
-
-const PLUGIN_NAME = 'posthog';
-
-console.log(`Plugin ${PLUGIN_NAME} is loaded`);
-
-setTimeout(() => {
-  console.log('Report plugin install status');
-
-  try {
-    const d = window.localStorage['$TailchatInstalledPlugins'];
-    if (!d) {
-      posthog.capture('Report Plugin', {
-        plugins: [],
-        pluginNum: 0,
-        pluginRaw: '',
-      });
-      return;
-    }
-    const storage = JSON.parse(d);
-    const list = storage.rawData;
-    if (!list || !Array.isArray(list)) {
-      // 格式不匹配
-      return;
-    }
-
-    posthog.capture('Report Plugin', {
-      plugins: list.map((item) => item.name), // 主要收集名称列表
-      pluginNum: list.length,
-      pluginRaw: JSON.stringify(list), // 原始信息
-    });
-  } catch (err) {
-    // Ignore error
-  }
-}, 2000);
-
-sharedEvent.on('loginSuccess', (userInfo) => {
-  posthog.identify(userInfo._id, {
-    email: userInfo.email,
-    username: `${userInfo.nickname}#${userInfo.discriminator}`,
-    avatar: userInfo.avatar,
-    temporary: userInfo.temporary,
+try {
+  posthog.init('phc_xRCv3qbbOBMQkz31kbYMngXxn7Ey5JMu0BZIFktO6km', {
+    api_host: 'https://app.posthog.com',
+    autocapture: false, // 关闭autocapture以节约事件用量
+    disable_session_recording: true, // 关闭自动录屏(不需要且一直报错)
   });
-});
+
+  const PLUGIN_NAME = 'posthog';
+
+  console.log(`Plugin ${PLUGIN_NAME} is loaded`);
+
+  setTimeout(() => {
+    console.log('Report plugin install status');
+
+    try {
+      const d = window.localStorage['$TailchatInstalledPlugins'];
+      if (!d) {
+        posthog.capture('Report Plugin', {
+          plugins: [],
+          pluginNum: 0,
+          pluginRaw: '',
+        });
+        return;
+      }
+      const storage = JSON.parse(d);
+      const list = storage.rawData;
+      if (!list || !Array.isArray(list)) {
+        // 格式不匹配
+        return;
+      }
+
+      posthog.capture('Report Plugin', {
+        plugins: list.map((item) => item.name), // 主要收集名称列表
+        pluginNum: list.length,
+        pluginRaw: JSON.stringify(list), // 原始信息
+      });
+    } catch (err) {
+      // Ignore error
+    }
+  }, 2000);
+
+  sharedEvent.on('loginSuccess', (userInfo) => {
+    posthog.identify(userInfo._id, {
+      email: userInfo.email,
+      username: `${userInfo.nickname}#${userInfo.discriminator}`,
+      avatar: userInfo.avatar,
+      temporary: userInfo.temporary,
+    });
+  });
+} catch (err) {
+  console.error(err);
+}
--- client/web/plugins/com.msgbyte.sentry/src/index.tsx
+++ client/web/plugins/com.msgbyte.sentry/src/index.tsx
@@ -2,24 +2,28 @@ import * as Sentry from '@sentry/react';
 import { BrowserTracing } from '@sentry/tracing';
 import { sharedEvent } from '@capital/common';
 
-Sentry.init({
-  dsn: 'https://177fd98a1e9e4deba84146a769633c32@o4504196236836864.ingest.sentry.io/4504196241293312',
-  replaysSessionSampleRate: 0,
-  replaysOnErrorSampleRate: 1.0,
-  integrations: [new BrowserTracing(), new Sentry.Replay()],
+try {
+  Sentry.init({
+    dsn: 'https://177fd98a1e9e4deba84146a769633c32@o4504196236836864.ingest.sentry.io/4504196241293312',
+    replaysSessionSampleRate: 0,
+    replaysOnErrorSampleRate: 1.0,
+    integrations: [new BrowserTracing(), new Sentry.Replay()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 0.1, // reduce sentry quota usage
-});
-
-sharedEvent.on('loginSuccess', (userInfo) => {
-  Sentry.setUser({
-    id: userInfo._id,
-    email: userInfo.email,
-    username: `${userInfo.nickname}#${userInfo.discriminator}`,
-    avatar: userInfo.avatar,
-    temporary: userInfo.temporary,
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.1, // reduce sentry quota usage
   });
-});
+
+  sharedEvent.on('loginSuccess', (userInfo) => {
+    Sentry.setUser({
+      id: userInfo._id,
+      email: userInfo.email,
+      username: `${userInfo.nickname}#${userInfo.discriminator}`,
+      avatar: userInfo.avatar,
+      temporary: userInfo.temporary,
+    });
+  });
+} catch (err) {
+  console.error(err);
+}
