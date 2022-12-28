ce8ee5954198926e14e51dfafbba0b6b36a846ff
moonrailgun <moonrailgun@gmail.com>
2022-12-28 17:55:04 +0800

    style: update w2a config

--- client/w2a/app.js
+++ client/w2a/app.js
@@ -1,12 +1,26 @@
 App({
   options: {
-    debug: false,
+    debug: true,
   },
   /**
    * 当wap2app初始化完成时，会触发 onLaunch
    */
   onLaunch: function () {
     console.log('launch');
+
+    uni.onPushMessage((res) => {
+      console.log('收到推送消息：', res); //监听推送消息
+    });
+
+    uni.getPushClientId({
+      success: (res) => {
+        let push_clientid = res.cid;
+        console.log('客户端推送标识:', push_clientid);
+      },
+      fail(err) {
+        console.log(err);
+      },
+    });
   },
   /**
    * 当wap2app启动，或从后台进入前台显示，会触发 onShow
@@ -21,6 +35,7 @@ App({
     console.log('hide');
   },
 });
+
 Page('__W2A__nightly.paw.msgbyte.com', {
   //首页扩展配置
   onShow: function () {},
