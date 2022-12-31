bc8abe54b7a1051d6b9e4ab5ebc629b6e065d776
moonrailgun <moonrailgun@gmail.com>
2022-12-31 22:35:03 +0800

    feat: 优化pluginUserExtraInfo渲染方式，增加自定义渲染组件自定义范围

--- client/web/src/components/popover/GroupUserPopover.tsx
+++ client/web/src/components/popover/GroupUserPopover.tsx
@@ -34,15 +34,19 @@ export const GroupUserPopover: React.FC<{
         <div className="pt-2">
           {pluginUserExtraInfo.map((item, i) => {
             const Component = item.component?.render;
+            if (Component) {
+              // 自定义渲染方式
+              return (
+                <Component key={item.name + i} value={userExtra[item.name]} />
+              );
+            }
+
+            // 默认渲染方式
             return (
               <div key={item.name + i} className="flex">
                 <div className="w-1/4 text-gray-500">{item.label}:</div>
                 <div className="w-3/4">
-                  {Component ? (
-                    <Component value={userExtra[item.name]} />
-                  ) : (
-                    String(userExtra[item.name])
-                  )}
+                  {userExtra[item.name] ? String(userExtra[item.name]) : ''}
                 </div>
               </div>
             );
