96292a23baacc39767a598a5976b08ef7e3e270c
moonrailgun <moonrailgun@gmail.com>
2023-01-09 18:31:50 +0800

    feat: admin 初始化与基本界面

---
+++ server/admin/app/ra/authProvider.ts
@@ -0,0 +1,33 @@
+import { AuthProvider } from 'react-admin';
+
+export const authProvider: AuthProvider = {
+  login: ({ username, password }) => {
+    // TODO
+    if (username !== 'admin' || password !== 'admin') {
+      return Promise.reject();
+    }
+    localStorage.setItem('username', username);
+    return Promise.resolve();
+  },
+  logout: () => {
+    localStorage.removeItem('username');
+    return Promise.resolve();
+  },
+  checkAuth: () =>
+    localStorage.getItem('username') ? Promise.resolve() : Promise.reject(),
+  checkError: (error) => {
+    const status = error.status;
+    if (status === 401 || status === 403) {
+      localStorage.removeItem('username');
+      return Promise.reject();
+    }
+    // other error code (404, 500, etc): no need to log out
+    return Promise.resolve();
+  },
+  getIdentity: () =>
+    Promise.resolve({
+      id: 'user',
+      fullName: 'John Doe',
+    }),
+  getPermissions: () => Promise.resolve(''),
+};
---
+++ server/admin/app/routes/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Index() {
+  // eslint-disable-next-line react/no-unescaped-entities
+  return <div>Please visit '/admin'</div>;
+}
