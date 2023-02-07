47d421a278c0e364e315ca0ee0852efdfb45a47e
moonrailgun <moonrailgun@gmail.com>
2023-02-07 18:02:42 +0800

    feat: 增加react-native-ui-lib，增加添加服务器功能

--- client/mobile/android/app/proguard-rules.pro
+++ client/mobile/android/app/proguard-rules.pro
@@ -8,3 +8,6 @@
 #   http://developer.android.com/guide/developing/tools/proguard.html
 
 # Add any project specific keep options here:
+
+-keep class com.swmansion.reanimated.** { *; }
+-keep class com.facebook.react.turbomodule.** { *; }
--- client/mobile/src/Entry.tsx
+++ client/mobile/src/Entry.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Alert, ScrollView, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, TextInput } from 'react-native';
 import { ServerCard } from './components/ServerCard';
 import { useServerStore } from './store/server';
+import Dialog from 'react-native-ui-lib/dialog';
+import { Button, PanningProvider, Text, View } from 'react-native-ui-lib';
 
 export const Entry: React.FC = React.memo(() => {
-  const { serverList, selectServer } = useServerStore();
+  const { serverList, selectServer, addServer } = useServerStore();
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const [serverUrl, setServerUrl] = useState('');
 
   return (
     <ScrollView style={styles.root}>
@@ -20,7 +24,32 @@ export const Entry: React.FC = React.memo(() => {
         );
       })}
 
-      <ServerCard name={'添加服务器'} onPress={() => Alert.alert('暂不支持')} />
+      <ServerCard name={'添加服务器'} onPress={() => setDialogVisible(true)} />
+
+      <Dialog
+        visible={dialogVisible}
+        panDirection={PanningProvider.Directions.DOWN}
+        onDismiss={() => setDialogVisible(false)}
+      >
+        <View backgroundColor="white" style={styles.dialog}>
+          <Text>输入服务器地址:</Text>
+
+          <TextInput
+            style={styles.textInput}
+            inputMode="url"
+            value={serverUrl}
+            onChangeText={setServerUrl}
+          />
+
+          <Button
+            label={'确认'}
+            onPress={() => {
+              addServer(serverUrl);
+              setDialogVisible(false);
+            }}
+          />
+        </View>
+      </Dialog>
     </ScrollView>
   );
 });
@@ -33,5 +62,18 @@ const styles = StyleSheet.create({
   },
   item: {
     marginBottom: 8,
+  },
+  dialog: {
+    borderRadius: 8,
+    padding: 10,
+  },
+  textInput: {
+    fontSize: 20,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginTop: 4,
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
   },
 });
