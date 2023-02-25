32875f8a72ec58a703e1cf760b40c310b3b8b235
moonrailgun <moonrailgun@gmail.com>
2023-02-25 16:56:02 +0800

    docs: upgrade homepage header style

--- website/src/components/HomepageHeader.tsx
+++ website/src/components/HomepageHeader.tsx
@@ -11,35 +11,40 @@ export const HomepageHeader: React.FC = React.memo(() => {
   const { colorMode } = useColorMode();
 
   return (
-    <div className="homepage-header hero hero--primary">
+    <div className="homepage-header">
       <Head>
         <link rel="prefetch" href="/img/hero-light.png" />
         <link rel="prefetch" href="/img/hero-dark.png" />
       </Head>
 
-      <div className="main">
-        <div className="header">
-          <h1 className="title">{siteConfig.title}</h1>
-          <p className="desc">{siteConfig.tagline}</p>
+      <div className="screenshot flex-1 xl:flex-none">
+        <img src={`/img/hero-${colorMode}.png`} alt="Preview of Tailchat" />
+      </div>
 
-          <div className="btns">
-            <Link
-              className="button button--info button--lg"
-              to="https://nightly.paw.msgbyte.com/"
-            >
-              <Translate>Nightly version</Translate>
-            </Link>
+      <div className="header">
+        <h1 className="title">Open Source, Open Platform</h1>
+        <p className="desc">Tailchat: {siteConfig.tagline}</p>
 
-            <Link
-              className="button button--secondary button--lg"
-              href="/docs/intro"
-            >
-              <Translate>Learn More</Translate>
-            </Link>
-          </div>
+        <div className="btns">
+          <Link
+            className="button button--primary button--lg"
+            to="https://nightly.paw.msgbyte.com/invite/8Jfm1dWb"
+          >
+            <Translate>Join our Group</Translate>
+          </Link>
+
+          <Link
+            className="button button--secondary button--lg"
+            href="/docs/intro"
+          >
+            <Translate>Learn More</Translate>
+          </Link>
         </div>
-        <div className="screenshot flex-1 xl:flex-none">
-          <img src={`/img/hero-${colorMode}.png`} alt="Preview of Tailchat" />
+
+        <div className="link">
+          <Link to="https://nightly.paw.msgbyte.com/">
+            <Translate>Or direct visit Tailchat nightly version</Translate>
+          </Link>
         </div>
       </div>
     </div>
