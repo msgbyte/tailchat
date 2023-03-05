e66591d022a6f009438980ab6d66a4dace6685d8
moonrailgun <moonrailgun@gmail.com>
2023-03-05 17:26:05 +0800

    fix: modify right position of ScrollToBottom
    make sure not overlapping with reply close button

--- client/web/src/components/ChatBox/ChatMessageList/ScrollToBottom.tsx
+++ client/web/src/components/ChatBox/ChatMessageList/ScrollToBottom.tsx
@@ -11,7 +11,7 @@ interface Props {
 export const ScrollToBottom: React.FC<Props> = React.memo((props) => {
   return (
     <div
-      className="absolute right-6 bottom-18 px-3 py-2 rounded-full bg-white dark:bg-black bg-opacity-50 shadow cursor-pointer z-10 w-11 h-11 flex justify-center items-center text-2xl hover:bg-opacity-80"
+      className="absolute right-10 bottom-18 px-3 py-2 rounded-full bg-white dark:bg-black bg-opacity-50 shadow cursor-pointer z-10 w-11 h-11 flex justify-center items-center text-2xl hover:bg-opacity-80"
       onClick={props.onClick}
     >
       <Icon icon="mdi:chevron-double-down" />
