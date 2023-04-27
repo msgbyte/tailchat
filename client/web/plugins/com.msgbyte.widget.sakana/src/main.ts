0c02849775c0fa767e32c91d6b78d48ee9cd9fce
moonrailgun <moonrailgun@gmail.com>
2023-04-27 15:00:04 +0800

    style: move sakana widget positon to bottom

--- client/web/plugins/com.msgbyte.widget.sakana/src/main.ts
+++ client/web/plugins/com.msgbyte.widget.sakana/src/main.ts
@@ -4,7 +4,7 @@ import './main.css'; // copy from sakana-widget/dist/index.css
 const container = document.createElement('div');
 container.style.position = 'fixed';
 container.style.left = '60px';
-container.style.bottom = '80px';
+container.style.bottom = '0px';
 
 document.body.appendChild(container);
 new SakanaWidget().setState({ r: 1, y: 1, t: 0, w: 0 }).mount(container);
