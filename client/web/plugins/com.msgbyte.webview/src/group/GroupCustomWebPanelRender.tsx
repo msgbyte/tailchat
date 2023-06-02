9965a54bdb64da1d921a34fc8ddd2a27e43d3239
moonrailgun <moonrailgun@gmail.com>
2023-06-02 13:51:30 +0800

    refactor: use file stream read and write instead of blobUrl to render custom html panel

--- client/web/plugins/com.msgbyte.webview/src/group/GroupCustomWebPanelRender.tsx
+++ client/web/plugins/com.msgbyte.webview/src/group/GroupCustomWebPanelRender.tsx
@@ -1,8 +1,5 @@
-import React, { useMemo } from 'react';
-import { isValidStr } from '@capital/common';
+import React, { useEffect, useRef } from 'react';
 import { Translate } from '../translate';
-import { sanitize } from 'script_sanitize';
-import { WebviewKeepAlive } from '@capital/component';
 
 function getInjectedStyle() {
   try {
@@ -17,51 +14,27 @@ function getInjectedStyle() {
   }
 }
 
-const cacheMap = new Map<string, string>();
-
-function createInlineUrl(html: string): string | undefined {
-  if (cacheMap.has(html)) {
-    return cacheMap.get(html);
-  }
-
-  if (isValidStr(html)) {
-    const appendHtml = getInjectedStyle(); // 额外追加的样式
-    try {
-      const blob = new Blob(
-        [
-          sanitize(html + appendHtml, {
-            replacementText: 'No allowed script',
-          }),
-        ],
-        {
-          type: 'text/html',
-        }
-      );
-
-      const url = window.URL.createObjectURL(blob);
-      cacheMap.set(html, url);
-      return url;
-    } catch (e) {
-      return undefined;
-    }
-  } else {
-    return undefined;
-  }
-}
-
 const GroupCustomWebPanelRender: React.FC<{ panelInfo: any }> = (props) => {
   const panelInfo = props.panelInfo;
+  const ref = useRef<HTMLIFrameElement>(null);
+  const html = panelInfo?.meta?.html;
+
+  useEffect(() => {
+    if (!ref.current || !html) {
+      return;
+    }
+
+    const doc = ref.current.contentWindow.document;
+    doc.open();
+    doc.writeln(getInjectedStyle(), html);
+    doc.close();
+  }, [html]);
 
   if (!panelInfo) {
     return <div>{Translate.notfound}</div>;
   }
 
-  const html = panelInfo?.meta?.html;
-  const url = useMemo(() => {
-    return createInlineUrl(html);
-  }, [html]);
-
-  return <WebviewKeepAlive className="w-full h-full" url={url} />;
+  return <iframe ref={ref} />;
 };
 GroupCustomWebPanelRender.displayName = 'GroupCustomWebPanelRender';
 
