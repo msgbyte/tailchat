fd9787f77bc6588eb0a6365e1e59843f3080cade
moonrailgun <moonrailgun@gmail.com>
2023-06-26 00:30:33 +0800

    perf: beautify dashboard chart color

--- server/admin/src/client/components/Dashboard.tsx
+++ server/admin/src/client/components/Dashboard.tsx
@@ -231,8 +231,8 @@ const UserCountChart: React.FC = React.memo(() => {
       >
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
           </linearGradient>
         </defs>
         <XAxis dataKey="date" />
@@ -243,7 +243,7 @@ const UserCountChart: React.FC = React.memo(() => {
           type="monotone"
           dataKey="count"
           label={t('custom.dashboard.newUserCount')}
-          stroke="#8884d8"
+          stroke="#82ca9d"
           fillOpacity={1}
           fill="url(#colorUv)"
         />
