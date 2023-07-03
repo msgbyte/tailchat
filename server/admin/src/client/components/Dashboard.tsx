9f6d10cb2829cdd211db8688d779d45fab37a89f
moonrailgun <moonrailgun@gmail.com>
2023-07-04 00:34:59 +0800

    fix(admin): fix dashboard color conflict problem

--- server/admin/src/client/components/Dashboard.tsx
+++ server/admin/src/client/components/Dashboard.tsx
@@ -230,7 +230,7 @@ const UserCountChart: React.FC = React.memo(() => {
         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
       >
         <defs>
-          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+          <linearGradient id="userCountColor" x1="0" y1="0" x2="0" y2="1">
             <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
             <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
           </linearGradient>
@@ -245,7 +245,7 @@ const UserCountChart: React.FC = React.memo(() => {
           label={t('custom.dashboard.newUserCount')}
           stroke="#82ca9d"
           fillOpacity={1}
-          fill="url(#colorUv)"
+          fill="url(#userCountColor)"
         />
       </AreaChart>
     </ResponsiveContainer>
@@ -275,7 +275,7 @@ const MessageCountChart: React.FC = React.memo(() => {
         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
       >
         <defs>
-          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+          <linearGradient id="messageCountColor" x1="0" y1="0" x2="0" y2="1">
             <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
             <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
           </linearGradient>
@@ -290,7 +290,7 @@ const MessageCountChart: React.FC = React.memo(() => {
           label={t('custom.dashboard.messageCount')}
           stroke="#8884d8"
           fillOpacity={1}
-          fill="url(#colorUv)"
+          fill="url(#messageCountColor)"
         />
       </AreaChart>
     </ResponsiveContainer>
