67ba667dfb92746e7acae7d3bbc37b5cb37f05d9
lihl <lihl@kamfu.com.cn>
2023-07-17 18:17:44 +0800

    fix: 修复播放

--- client/web/src/components/Card/FileCard.tsx
+++ client/web/src/components/Card/FileCard.tsx
@@ -1,7 +1,7 @@
 import { downloadUrl } from '@/utils/file-helper';
 import React from 'react';
 import { Icon } from 'tailchat-design';
-import { useMemoizedFn, t } from 'tailchat-shared';
+import { useMemoizedFn, t, parseUrlStr } from 'tailchat-shared';
 import { IconBtn } from '../IconBtn';
 import { CardWrapper } from './Wrapper';
 
@@ -18,6 +18,41 @@ export const FileCard: React.FC<{
   const handleDownload = useMemoizedFn(() => {
     downloadUrl(payload.url, payload.label);
   });
+  let videoShow = true;
+  const showVideo = useMemoizedFn(() => {
+    videoShow = false;
+  });
+
+  const fileType = payload.url.substring(payload.url.lastIndexOf('.') + 1);
+
+  if (fileType === 'mp4' || fileType === 'webm' || fileType === 'ogg') {
+    return (
+      <CardWrapper>
+        <div>
+          <div className="flex w-full items-center">
+            <div className="mr-3 overflow-hidden flex-1">
+              <div className="text-sm text-black text-opacity-60 dark:text-white dark:text-opacity-60">
+                {payload.label}
+              </div>
+            </div>
+            <IconBtn
+              title={t('下载')}
+              icon="mdi:cloud-download-outline"
+              onClick={handleDownload}
+            />
+          </div>
+          <br />
+          <video
+            src={parseUrlStr(payload.url)}
+            width={300}
+            controls
+            autoPlay={false}
+            preload={'none'}
+          ></video>
+        </div>
+      </CardWrapper>
+    );
+  }
 
   return (
     <CardWrapper>
