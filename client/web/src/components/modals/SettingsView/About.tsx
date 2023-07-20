542e0292ca04659a40715ad879fbb6e80eca98db
moonrailgun <moonrailgun@gmail.com>
2023-07-20 23:29:00 +0800

    fix: fix i18n problem in about panel

--- client/web/src/components/modals/SettingsView/About.tsx
+++ client/web/src/components/modals/SettingsView/About.tsx
@@ -1,6 +1,6 @@
 import { Typography } from 'antd';
 import React from 'react';
-import { version } from 'tailchat-shared';
+import { t, version } from 'tailchat-shared';
 import logoUrl from '@assets/images/logo.svg';
 import { Icon } from 'tailchat-design';
 
@@ -37,21 +37,23 @@ export const SettingsAbout: React.FC = React.memo(() => {
 
       <Paragraph>
         <Text className="font-bold">Tailchat</Text>
-        <Text>: 属于所有人的开源聊天工具</Text>
+        <Text>: {t('属于所有人的开源聊天工具')} </Text>
       </Paragraph>
-      <Paragraph>可供高度自定义的聊天工具</Paragraph>
-      <Paragraph>完全独属于私人团队的沟通平台</Paragraph>
+      <Paragraph>{t('可供高度自定义的聊天工具')}</Paragraph>
+      <Paragraph>{t('完全独属于私人团队的沟通平台')}</Paragraph>
 
       <Paragraph>
-        <div>特性(亮点):</div>
+        <div>{t('特性(亮点)')}:</div>
         <ul className="list-disc list-inside">
-          <li>基于面板的群组空间, 可高度自定义化</li>
-          <li>基于微内核的前端插件支撑, 私人定制化</li>
-          <li>分布式部署可供任意规模的使用需求</li>
+          <li>{t('基于面板的群组空间, 可高度自定义化')}</li>
+          <li>{t('基于微内核的前端插件支撑, 私人定制化')}</li>
+          <li>{t('分布式部署可供任意规模的使用需求')}</li>
         </ul>
       </Paragraph>
 
-      <Paragraph>当前版本: {version}</Paragraph>
+      <Paragraph>
+        {t('当前版本')}: {version}
+      </Paragraph>
 
       <div className="text-8xl flex flex-wrap space-x-4 bg-black p-4 rounded-md bg-opacity-20">
         <LogoLink
@@ -81,7 +83,7 @@ export const SettingsAbout: React.FC = React.memo(() => {
       </div>
 
       <Paragraph className="mt-4">
-        开源地址:{' '}
+        {t('开源地址')}:{' '}
         <Link href="https://github.com/msgbyte/tailchat" target="_blank">
           https://github.com/msgbyte/tailchat
         </Link>
