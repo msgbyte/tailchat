f29352dd87118d45930682e9cf180f4347d0507c
moonrailgun <moonrailgun@gmail.com>
2023-07-29 02:30:12 +0800

    refactor(cli): update template config

--- apps/cli/templates/server-plugin-full/{{id}}/.ministarrc.js
+++ apps/cli/templates/server-plugin-full/{{id}}/.ministarrc.js
@@ -1,7 +1,17 @@
 const path = require('path');
 
+const pluginRoot = path.resolve(__dirname, './web');
+const outDir = path.resolve(__dirname, '../../public');
+
 module.exports = {
-  externalDeps: ['react', 'react-router', 'axios', 'styled-components', 'zustand', 'zustand/middleware/immer'],
-  pluginRoot: path.resolve(__dirname, './web'),
-  outDir: path.resolve(__dirname, '../../public'),
+  externalDeps: [
+    'react',
+    'react-router',
+    'axios',
+    'styled-components',
+    'zustand',
+    'zustand/middleware/immer'
+  ],
+  pluginRoot,
+  outDir,
 };
