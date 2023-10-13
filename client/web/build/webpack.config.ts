92828b756581c37d3b27e51a69c23afc23c4a8bf
moonrailgun <moonrailgun@gmail.com>
2023-10-14 00:26:01 +0800

    chore: add netlify redirect

--- client/web/build/webpack.config.ts
+++ client/web/build/webpack.config.ts
@@ -81,6 +81,10 @@ const plugins: Configuration['plugins'] = [
         to: 'pwa.webmanifest',
       },
       {
+        from: path.resolve(ROOT_PATH, './assets/_redirects'),
+        to: './', // for netlify
+      },
+      {
         from: path.resolve(ROOT_PATH, './assets/images/logo/'),
         to: 'images/logo/',
       },
