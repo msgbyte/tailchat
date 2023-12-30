341c260d6f4d3fc53403aca95e765a34d1f576ff
moonrailgun <moonrailgun@gmail.com>
2023-12-31 01:12:41 +0800

    test: update unit test case for preprocessMessage

--- client/web/src/components/ChatBox/__tests__/preprocessMessage.spec.ts
+++ client/web/src/components/ChatBox/__tests__/preprocessMessage.spec.ts
@@ -2,11 +2,19 @@ import { regMessageTextDecorators } from '@/plugin/common';
 import { preprocessMessage } from '../preprocessMessage';
 
 regMessageTextDecorators(() => ({
-  emoji: (code) => `[emoji]${code.substring(1, code.length - 1)}[/emoji]`,
+  emoji: (code) => `[emoji]${code}[/emoji]`,
 }));
 
-test('preprocessMessage', () => {
-  expect(preprocessMessage('anystring :face: anystring :heart:')).toBe(
-    'anystring [emoji]face[/emoji] anystring [emoji]heart[/emoji]'
-  );
+describe('preprocessMessage', () => {
+  test('should be transform emoji code', () => {
+    expect(preprocessMessage('anystring :robot_face: anystring :heart:')).toBe(
+      'anystring [emoji]robot_face[/emoji] anystring [emoji]heart[/emoji]'
+    );
+  });
+
+  test('should ignore non-emoji code between ::', () => {
+    expect(preprocessMessage('2023-12-31 01:11:17')).toBe(
+      '2023-12-31 01:11:17'
+    );
+  });
 });
